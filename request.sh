ef0fdbcd239ab37809e9fba9fa9e254a29b50775
Jordan Penard <jordan.penard@thinci.com>
2019-11-21 17:09:56 +0000

    Bug fix, now can run even if the user isn't in the same dir as the scripts

--- request.sh
+++ request.sh
@@ -4,5 +4,7 @@ from=$1
 to=$2
 number=$3
 
-data=`cat request_template.xml | sed "s/%from%/${from}/" | sed "s/%to%/${to}/" | sed "s/%number%/${number}/"`
+path=`dirname $0`
+
+data=`cat ${path}/request_template.xml | sed "s/%from%/${from}/" | sed "s/%to%/${to}/" | sed "s/%number%/${number}/"`
 curl -s --header "Content-Type: text/xml;charset=UTF-8" --data "$data" https://lite.realtime.nationalrail.co.uk/OpenLDBWS/ldb9.asmx
